009c496fb0874efc0774f85ccee441cf4bf42b9a
Sajjad <Sajjad@DESKTOP-4A8KQPV>
2019-04-07 17:00:29 +0430

    first commit

---
+++ ClouldProject-2/src/main/java/com/example/election/Election.java
@@ -0,0 +1,43 @@
+package com.example.election;
+
+import java.util.Date;
+import java.util.List;
+public class Election {
+	int Id;
+	Date startTime;
+	Date endTime;
+	List<String> listOfChoice;
+	int numberOfVotes;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	public Date getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	public List<String> getListOfChoice() {
+		return listOfChoice;
+	}
+	public void setListOfChoice(List<String> listOfChoice) {
+		this.listOfChoice = listOfChoice;
+	}
+	public int getNumberOfVotes() {
+		return numberOfVotes;
+	}
+	public void setNumberOfVotes(int numberOfVotes) {
+		this.numberOfVotes = numberOfVotes;
+	}
+
+	
+}
---
+++ ClouldProject-2/src/main/java/com/example/server/main/ClouldProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouldProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClouldProject2Application.class, args);
+	}
+
+}
